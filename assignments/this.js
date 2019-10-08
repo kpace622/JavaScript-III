3cc1bc1b18f1b0d27959f2a02bbb1dff656d98ba
Kyle Pace <kpace2296@gmail.com>
2019-10-07 22:42:03 -0500

    four principles

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,14 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.  this always refers to the object that it is in so if you don't specify an object it will assume you want the window, basically how the browser works
+
+* 2.  Implicit Binding. This refers to the object before the dot. This is the easiest one to think about when you are holding something in your hand and you say look at "this" you are refering to something. 
+    That is how I see implicit binding.
+
+* 3.  New Binding. New refers specifically to constructor functions. You have key value pairs that are coded as this.something. You can then write code using those key value pairs
+
+* 4.  Explicit binding. Whenever you use .call or .apply. This also has to do with constructor functions. We can change objects using .call or .apply.
 *
 * write out a code example of each explanation above
 */
