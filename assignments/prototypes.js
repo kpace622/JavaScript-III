3632c0f192608d6286dc84ff13892e16c5e250d0
Kyle Pace <kpace2296@gmail.com>
2019-10-08 19:12:04 -0500

    started stretch

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -128,7 +128,10 @@ Humanoid.prototype.greet = function() {
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
-
+  function Villain(villainAttrs) {
+    humanoidAttrs.call(this, villainAttrs);
+  }
+  villain.prototype
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
